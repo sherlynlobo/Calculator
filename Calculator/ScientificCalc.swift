54d6c27dbcfbbabba64268a1929a45dc7e60a6e1
Sherlyn Lobo <sherlynlobo@macbook-pro-2.local>
2018-10-18 15:23:47 -0400

    Made Scientific Calculator UI

---
+++ Calculator/ScientificCalc.swift
@@ -0,0 +1,43 @@
+//
+//  ScientificCalc.swift
+//  Calculator
+//
+//  Created by Sherlyn Lobo on 2018-10-18.
+//  Copyright © 2018 Sherlyn Lobo. All rights reserved.
+
+//  ViewController.swift
+//  Calculator
+//  Created by Sherlyn Lobo on 2018-09-25.
+//  Copyright © 2018 Sherlyn Lobo. All rights reserved.
+
+//  File Name       : ViewController.swift
+//  Author Name     : Sherlyn Lobo
+//  Student ID      : 301013071
+//  Date            : 26-09-2018 (DD/MM/YYYY)
+//  App Description : Simple Calculator
+//  Version : 3.1
+
+import Foundation
+import UIKit
+//View Controller class
+class ScientificCalc: UIViewController {
+    
+    
+@IBOutlet var ResultsLabel: UILabel!
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
